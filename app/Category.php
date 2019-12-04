6f4986038f96a9167dea26fcfb7adcd82707e8e7
Abel Aguiar <abel.prog@gmail.com>
2019-12-04 10:04:12 -0300

    Atualização de informações model Products
    Nome da tabela é coloco de forma automática, apartir do nome do model, assim não precisa coloca mais informações quanto a isso.

--- app/Category.php
+++ app/Category.php
@@ -6,16 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $table = 'categories';
-
-    protected $attributes = [
-        'name' => ""
-    ];
     protected $fillable = ['name'];
+
     protected $guarded = ['id', 'created_at', 'update_at'];
 
     public function products()
     {
-        return $this->hasMany('App\Product')->withTimestamps();
+        return $this->hasMany(Product::class)->withTimestamps();
     }
 }
