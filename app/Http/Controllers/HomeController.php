7eac1aef4e7a6d73ef80cf2571ae1b675cb61fb5
Abel Aguiar <abel.prog@gmail.com>
2019-12-04 09:50:40 -0300

    Lista de produtos
    Você pode chamar o model dentro de qualquer controller, a parte de authorization pode ser colocada nas rotas.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,20 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use DB;
+use App\Product;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
@@ -25,7 +15,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $products = DB::table('products')->get();
+        $products = Product::all();
+
         return view('home')->with('products', $products);
     }
 }
